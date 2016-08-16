985f320dc0f14215a1aa1cab824cf438a77d006d
Robb Marsee <robb.marsee@gamil.com>
2016-08-15 20:02:35 -0700

    update server to take events and photo api

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -2,6 +2,32 @@ var request = require('request');
 var initLon = null;
 var initLat = null;
 
+// var eventsCategory = {
+//   business: 'conference,business',
+//   family: 'family_fun_kids',
+//   comedy: 'comedy',
+//   festivals: 'festivals_parades,food,outdoors_recreation',
+//   sports: 'sports',
+//   music: 'music',
+//   social: 'attractions,community,singles_social',
+//   film: 'movies_film',
+//   art: 'art,performing_arts',
+//   sci_tec: 'science+technology',
+// };
+
+// var placesKeyword = {
+//   hotels: 'lodging',
+//   cafe: 'bakery+cafe',
+//   parks: 'park',
+//   bars: 'bar+night_club',
+//   shopping: 'clothing_store+department_store+electronics_store+shopping_mall',
+//   food: 'restaurant',
+//   public_transit: 'bus_station+subway_station+train_station',
+//   banks_atm: 'atm+bank',
+//   parking: 'parking',
+//   gas_stations: 'gas_station',
+// };
+
 function deg2rad(deg) {
   return deg * (Math.PI / 180);
 }
@@ -35,21 +61,24 @@ function findYDistance(hypotenuse, xDistance, initLon, newLon) {
   return (newLon - initLon > 0 ) ? yDistance : -1 * yDistance;
 }
 
-function getPlaces(req, res) {
-  // check to see if it is the initial location
+function checkInit(req) {
   if (req.body.threejsLat === 0 && req.body.threejsLon === 0) {
-    // if so recored the initial position
     initLat = req.body.latitude;
     initLon = req.body.longitude;
   }
+}
+
+function getPlaces(req, res) {
+  // check to see if it is the initial location
+  checkInit(req);
   var googleType = '';
   var googleKeyword = '';
   var googleOpenNow = '';
   if (req.body.type !== undefined) {
-    googleType = `&type=${req.body.type}`;
+    googleType = `&type=${req.body.type.join('+')}`;
   }
   if (req.body.keyword !== undefined) {
-    googleKeyword = `&keyword=${req.body.keyword}`;
+    googleKeyword = `&keyword=${req.body.keyword.join('+')}`;
   }
   if (req.body.openNow !== undefined) {
     googleOpenNow = '&opennow';
@@ -88,25 +117,41 @@ function getPlaces(req, res) {
     });
   });
 }
+function dateFormat(date) {
+  var day = date.getDate();
+  if ((date.getDate()) <= 9) {
+    day = `0${date.getDate()}`;
+  }
+  var month = date.getMonth() + 1;
+  if ((date.getMonth() + 1) <= 9) {
+    month = `0${date.getMonth() + 1}`;
+  }
+  return `${date.getFullYear()}${month}${day}00`;
+}
 
 function getEvents(req, res) {
-  if (req.body.threejsLat === 0 && req.body.threejsLon === 0) {
-    // if so recored the initial position
-    initLat = req.body.latitude;
-    initLon = req.body.longitude;
-  }
+  checkInit(req);
   var date = 'Future';
+  // var startDate = new Date();
+  // var endDate = new Date();
+  // endDate.setDate(endDate.getDate() + req.body.date);
+  // var date = `${dateFormat(startDate())}-${dateFormat(endDate())}`;
   var eventsApiKey = 'CbNBBV9Qm4wTwMpg';
-  var radius = 1;
-  var eventsApiLink = `http://api.eventful.com/json/events/search?...&location=${req.body.latitude},${req.body.longitude}&within=${radius}&units=miles&date=${date}&app_key=${eventsApiKey}`;
-  // http://api.eventful.com/json/events/search?...&location=37.783537,-122.409003&within=.25&units=miles&date=Future&CbNBBV9Qm4wTwMpg
-  // http://api.eventful.com/json/events/search?...&location=37.783537,-122.409003&within=.25&units=miles&date=Future&app_key=CbNBBV9Qm4wTwMpg
+  var radius = .25;
+  var category = '';
+  if (req.body.category !== undefined) {
+    category = `&category=${req.body.category.join(',')}`;
+  }
+  var keyword = '';
+  if (req.body.keyword !== undefined) {
+    category = `&keyword=${req.body.category.join(',')}`;
+  }
+  var eventsApiLink = `http://api.eventful.com/json/events/search?...&location=${req.body.latitude},${req.body.longitude}&within=${radius}&units=miles&date=${date}${keyword}${category}&app_key=${eventsApiKey}`;
   return new Promise((resolve, reject) => {
     request(eventsApiLink, function(error, response, body) {
       if (!error && response.statusCode === 200) {
         var eventObj = [];
         var eventsResults = JSON.parse(body);
-
         eventsResults.events.event.forEach(function(event) {
           var eventLat = findXDistance(initLat, event.latitude);
           var distanceFromInit = hypotenuseDistance(initLat, initLon, event.latitude, event.longitude);
@@ -129,7 +174,38 @@ function getEvents(req, res) {
   });
 }
 
+function getVenueID(req, res) {
+  var flickrApiKey = 'd06a651de2c308348be5326769d1ce47';
+  var flickrIdApiLink = `https://api.flickr.com/services/rest/?method=flickr.people.findByUsername&api_key=${flickrApiKey}&username=${req.body.username.join('+')}`;
+  return new Promise((resolve, reject) => {
+    request(flickrIdApiLink, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        var result = JSON.parse(body);
+        getPhotos(req, res, result.user.id);
+      }
+    });
+  });
+}
+
+function getPhotos(req, res, userId) {
+  var flickrApiKey = 'd06a651de2c308348be5326769d1ce47';
+  var flickrGetPhotosLink = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${flickrApiKey}&user_id=${userId}&format=json&nojsoncallback=1`;
+  return new Promise((resolve, reject) => {
+    request(flickrGetPhotosLink, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        var results = JSON.parse(body);
+        var photoLinks = [];
+        results.photos.photo.forEach(function(photo) {
+          photoLinks.push(`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`);
+        });
+      }
+      resolve(res.send(photoLinks));
+    });
+  });
+}
+
 module.exports = {
   getPlaces,
-  getEvents
+  getEvents,
+  getVenueID,
 };
--- server/router.js
+++ server/router.js
@@ -2,5 +2,7 @@ var router = require('express').Router();
 var controller = require('./controller/controller');
 
 router.post('/location', controller.getPlaces);
+router.post('/places', controller.getPlaces);
+router.post('/events', controller.getEvents);
 
 module.exports = router;
