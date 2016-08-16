6699ec55dc6f6318bae2a568bde2b70022e44110
Robb Marsee <robb.marsee@gamil.com>
2016-08-16 12:46:08 -0700

    update server

--- server/router.js
+++ server/router.js
@@ -2,7 +2,7 @@ var router = require('express').Router();
 var controller = require('./controller/controller');
 
 router.post('/location', controller.getPlaces);
-router.post('/places', controller.getPlaces);
-router.post('/events', controller.getEvents);
+// router.post('/places', controller.getPlaces);
+// router.post('/events', controller.getEvents);
 
 module.exports = router;
