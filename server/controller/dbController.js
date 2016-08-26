e871c1cef6c2fa90fbfc09e7719d2ac1d5c1bd50
Robb Marsee <robb.marsee@gamil.com>
2016-08-26 10:54:05 -0700

    udpate for redis

--- server/controller/dbController.js
+++ server/controller/dbController.js
@@ -1,11 +1,4 @@
-if (process.env.REDISTOGO_URL) {
-  var rtg   = require('url').parse(process.env.REDISTOGO_URL);
-  var redis = require('redis').createClient(rtg.port, rtg.hostname);
-
-redis.auth(rtg.auth.split(':')[1]);
-} else {
-    var redis = require('redis').createClient();
-}
+var redis = require('../router');
 
 var counter = 1;
 
