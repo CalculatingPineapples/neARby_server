7c6bb3056c8a8a0fd0da4ac8988b3dee7707d28b
Robb Marsee <robb.marsee@gamil.com>
2016-08-26 10:56:34 -0700

    update redis server

--- server/server.js
+++ server/server.js
@@ -15,11 +15,12 @@ app.use('/', router);
 app.listen(process.env.PORT || port);
 console.log(`Listening on port ${port}`);
 
-// if (process.env.REDISTOGO_URL){
-//   var rtg = require('url').parse(process.env.REDISTOGO_URL);
-//   redis = require('redis').createClient(rtg.port, rtg.hostname);
-// } else {
-//   redis = require('redis').createClient();
-// }
+if (process.env.REDISTOGO_URL) {
+  var rtg   = require('url').parse(process.env.REDISTOGO_URL);
+  var redis = require('redis').createClient(rtg.port, rtg.hostname);
+  redis.auth(rtg.auth.split(':')[1]);
+} else {
+    var redis = require('redis').createClient();
+}
 
-// module.exports = redis;
+module.exports = redis;
